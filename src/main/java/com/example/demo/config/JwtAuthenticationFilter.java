7e74d7fb6c5a6e68c301f116c79cc796fce993ed
dineshpatte <152471120+dineshpatte@users.noreply.github.com>
2025-11-29 12:05:01 +0530

    jwtUtils and filteration done

--- src/main/java/com/example/demo/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/config/JwtAuthenticationFilter.java
@@ -1,15 +1,52 @@
 package com.example.demo.config;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-public class JwtAuthenticationFilter {
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Autowired
     private jwtUtil jwtUtil;
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response , FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        String token = null;
+        String username = null;
+
+        if(authHeader != null && authHeader.startsWith("Bearer ")) {
+            token = authHeader.substring(7);
+            username = jwtUtil.getUsername(token);
+        }
+
+        if(username!=null&& SecurityContextHolder.getContext().getAuthentication()==null) {
+
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+
+           if(jwtUtil.validateToken(token,userDetails)) {
+               UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+
+               SecurityContextHolder.getContext().setAuthentication(authToken);
+           }
+        }
+
+        filterChain.doFilter(request,response);
+    }
 
 
 }
--- src/main/java/com/example/demo/config/jwtUtil.java
+++ src/main/java/com/example/demo/config/jwtUtil.java
@@ -1,5 +1,7 @@
 package com.example.demo.config;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.Filter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -8,16 +10,50 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.security.Key;
+import java.util.Date;
+
 
 @Configuration
 public class jwtUtil {
 
+    private final String SECRET = "secret";
+
+    private final long EXPIRATION = 1000*60*60*10;
 
 
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
 
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder().setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis()+EXPIRATION))
+                .signWith(getSigningKey())
+                .compact();
+
+    }
+
+
+    public String getUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET)
+                .build()
+                .parseClaimsJws(token).getBody().getSubject();
+
+
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        String username = getUsername(token);
+        return(username.equals(userDetails.getUsername()));
+    }
 }
